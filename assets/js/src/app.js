f081eb6eedcd451d1c3526d38029f3c48c92e9d2
Nickolas Kenyeres <nkenyeres@gmail.com>
2013-11-09 13:11:56 -0500

    Moved assets from ui folder.

---
+++ assets/js/src/app.js
@@ -0,0 +1,18 @@
+// Set path to icons
+L.Icon.Default.imagePath = '/images';
+
+// create a map in the "map" div, set the view to a given place and zoom
+var map = L.map('map').setView([43.172994, -79.236745], 13);
+
+
+// create a new tile layer
+var tileUrl = 'http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png',
+    layer = new L.TileLayer(tileUrl, {maxZoom: 18});
+
+// add the layer to the map
+map.addLayer(layer);
+
+// add a marker in the given location, attach some popup content to it and open the popup
+L.marker([43.172994, -79.236745]).addTo(map)
+    .bindPopup('The centre of the universe')
+    .openPopup(); 
